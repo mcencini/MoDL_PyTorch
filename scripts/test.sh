c0811537a181290aaa3ab5bacaccf9e877d7fd55
Matteo Cencini <mcencini@aleatico1.imago7.local>
2023-10-06 12:14:21 +0200

    Update test code

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
-GPU_NUM=1
-TEST_CONFIG_YAML="configs/base_modl,k=10.yaml"
+GPU_NUM=0
+TEST_CONFIG_YAML="configs/base_modl,k=1.yaml"
 
 CUDA_VISIBLE_DEVICES=$GPU_NUM python test.py \
     --config=$TEST_CONFIG_YAML \
     --batch_size=32 \
-    --write_image=1
+    --write_image=10
